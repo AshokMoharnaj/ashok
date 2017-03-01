819a62ced3845cc639f377b6525ef919b2dea83e
AshokMoharnaj <ashok.mohanraj@citi.com>
2017-03-01 11:43:35 +0530

    update 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package com.citi.demo;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-	      // Prints "Hello, World" in the terminal window. test
+	      // Prints "Hello, World" in the terminal window.
 	      System.out.println("Hello, World");
 	   }
 }
